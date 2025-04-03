fa2e6b52f27863db86a5fb66b1182204b9095850
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2025-04-03 11:01:10 +0800

    Add fonts

--- src/App.js
+++ src/App.js
@@ -19,9 +19,41 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="text-template" style={{ padding: '20px' }}>
+      <h1>This is an H1 Heading (30px, Bold)</h1>
+      <h2>This is an H2 Heading (24px, Bold)</h2>
+      <h3>This is an H3 Heading (20px, Bold)</h3>
+      
+      <p>This is regular body text (18px). Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac tortor vitae purus faucibus ornare. Vivamus quis mi diam.</p>
+      
+      <figure style={{ marginTop: '20px' }}>
+        <div style={{ 
+          width: '300px', 
+          height: '150px', 
+          backgroundColor: '#eee', 
+          display: 'flex', 
+          alignItems: 'center', 
+          justifyContent: 'center' 
+        }}>
+          [Image Placeholder]
+        </div>
+        <figcaption>This is a figcaption element for image captions (15px)</figcaption>
+      </figure>
+      
+      <div style={{ marginTop: '20px' }}>
+        <div style={{ 
+          width: '300px', 
+          height: '150px', 
+          backgroundColor: '#eee', 
+          display: 'flex', 
+          alignItems: 'center', 
+          justifyContent: 'center' 
+        }}>
+          [Another Image Placeholder]
+        </div>
+        <div className="caption">This is a div with caption class for image captions (15px)</div>
+      </div>
+    </div>
   );
 }
 

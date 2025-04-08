169c611c12a9639b1e29d3db6c85707838543bdf
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2025-04-08 13:31:26 +0800

    Split up treatments

--- src/components/Treatments/Treatments.js
+++ src/components/Treatments/Treatments.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
-import medicalConditions from "../../data/medicalConditions.js";
+import { medicalConditions, medicalConditionsFlat } from "../../data/medicalConditions.js";
 
 // TODO: Split into Problems of Well Babies and Problems requiring intensive care
 
@@ -57,31 +57,48 @@ const Treatments = () => {
     <div className="p-4 lg:max-w-[70%] mx-auto">
       <div className="relative">
       <h1 className="mb-2 text-left md:text-left">
-          Services and Treatments
+        Conditions Treated
       </h1>
       {/* Add content for treatments here */}
-      {isMobileView ? (
-          <div className="overflow-x-auto mx-[-32px]">
-            <div className="flex flex-nowrap px-[20px]">
-              {medicalConditions.map((condition) => (
-                <div className="flex-shrink-0 w-[300px] md:w-1/2 lg:w-1/3 p-2" key={condition.id}>
-                  <TreatmentCard condition={condition} />
-                </div>
-              ))}
-            </div>
+
+      <div className="flex flex-wrap justify-left text-left">
+        {medicalConditions.map((category, index) => (
+          <div className="w-full mb-4" key={index}>
+            {category.categoryName && (
+              <h2 className="mb-2">{category.categoryName}</h2>
+            )}
+            {category.sections.map((section, sectionIndex) => (
+              <div key={sectionIndex} className="mb-4">
+                {section.sectionName && (
+                  <h3 className="mb-2">{section.sectionName}</h3>
+                )}
+                {isMobileView ? (
+                  <div className="overflow-x-auto mx-[-32px]">
+                    <div className="flex flex-nowrap px-[20px]">
+                      {section.problems.map((condition) => (
+                        <div className="flex-shrink-0 w-[300px] md:w-1/2 lg:w-1/3 p-2" key={condition.id}>
+                          <TreatmentCard condition={condition} />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ) : (
+                  <ResponsiveMasonry
+                    columnsCountBreakPoints={{300: 1, 600: 2, 900: 3, 1200: 4}}
+                    gutterBreakpoints={{350: "12px", 750: "16px", 900: "24px"}}
+                  >
+                    <Masonry>
+                      {section.problems.map((condition, _) => (
+                        <TreatmentCard condition={condition} onSeeMore={(id) => console.log(`See more about condition with id: ${id}`)} key={condition.id} />
+                      ))}
+                    </Masonry>
+                  </ResponsiveMasonry>
+                )}
+              </div>
+            ))}
           </div>
-        ) : (
-          <ResponsiveMasonry
-            columnsCountBreakPoints={{300: 1, 600: 2, 900: 3, 1200: 4}}
-            gutterBreakpoints={{350: "12px", 750: "16px", 900: "24px"}}
-          >
-            <Masonry>
-              {medicalConditions.map((condition, _) => (
-                <TreatmentCard condition={condition} onSeeMore={(id) => console.log(`See more about condition with id: ${id}`)} key={condition.id} />
-              ))}
-            </Masonry>
-          </ResponsiveMasonry>
-        )}
+        ))}
+      </div>
       </div>
     </div>
     </section>

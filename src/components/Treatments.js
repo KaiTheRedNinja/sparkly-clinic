3e864e80303d681679eb69c9c6382b34295772a3
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2025-04-10 12:34:04 +0800

    Use conditional rendering instead of javascript for mobile scroll view

--- src/components/Treatments.js
+++ src/components/Treatments.js
@@ -88,23 +88,10 @@ const PopoverSheet = ({ condition, onClose }) => {
 }
 
 const Treatments = () => {
-  // Handle mobile view
-  const isMobile = typeof window !== 'undefined' ? window.innerWidth < 768 : false;
-  const [windowWidth, setWindowWidth] = React.useState(isMobile ? 0 : 1000);
-
   const [selectedItem, setSelectedItem] = useState(null);
-  const sheetRef = useRef(null);
   const history = window.history;
 
   useEffect(() => {
-    // Resize handlers
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    
-    window.addEventListener('resize', handleResize);
-    handleResize();
-
     // URL change handlers
     const handleUrlChange = () => {
       const hash = window.location.hash;
@@ -125,13 +112,8 @@ const Treatments = () => {
     // Listen for hash changes
     window.addEventListener('hashchange', handleUrlChange);
     
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      window.removeEventListener('hashchange', handleUrlChange);
-    };
+    return () => window.removeEventListener('hashchange', handleUrlChange);
   }, []);
-
-  const isMobileView = windowWidth < 768;
 
   // Handle item selection
   const handleItemClick = (item) => {
@@ -166,28 +148,29 @@ const Treatments = () => {
                 {section.sectionName && (
                   <h3 className="mb-2">{section.sectionName}</h3>
                 )}
-                {isMobileView ? (
-                  <div className="overflow-x-auto mx-[-32px]">
-                    <div className="flex flex-nowrap px-[20px]">
-                      {section.problems.map((condition) => (
-                        <div className="flex-shrink-0 w-[300px] md:w-1/2 lg:w-1/3 p-2" key={condition.id}>
-                          <TreatmentCard condition={condition} onSeeMore={(_) => handleItemClick(condition)} />
-                        </div>
-                      ))}
-                    </div>
+                {/* Mobile scroll view */}
+                <div className="hidden sm:flex overflow-x-auto mx-[-32px]">
+                  <div className="flex flex-nowrap px-[20px]">
+                    {section.problems.map((condition) => (
+                      <div className="flex-shrink-0 w-[300px] md:w-1/2 lg:w-1/3 p-2" key={condition.id}>
+                        <TreatmentCard condition={condition} onSeeMore={(_) => handleItemClick(condition)} />
+                      </div>
+                    ))}
                   </div>
-                ) : (
-                  <ResponsiveMasonry
-                    columnsCountBreakPoints={{300: 1, 600: 2, 900: 3, 1200: 4}}
-                    gutterBreakpoints={{350: "12px", 750: "16px", 900: "24px"}}
-                  >
-                    <Masonry>
-                      {section.problems.map((condition, _) => (
-                        <TreatmentCard condition={condition} onSeeMore={(_) => handleItemClick(condition)} key={condition.id} />
-                      ))}
-                    </Masonry>
-                  </ResponsiveMasonry>
-                )}
+                </div>
+
+                {/* Desktop masonry grid */}
+                <ResponsiveMasonry
+                  columnsCountBreakPoints={{300: 1, 600: 2, 900: 3, 1200: 4}}
+                  gutterBreakpoints={{350: "12px", 750: "16px", 900: "24px"}}
+                  className="sm:hidden"
+                >
+                  <Masonry>
+                    {section.problems.map((condition, _) => (
+                      <TreatmentCard condition={condition} onSeeMore={(_) => handleItemClick(condition)} key={condition.id} />
+                    ))}
+                  </Masonry>
+                </ResponsiveMasonry>
               </div>
             ))}
           </div>
